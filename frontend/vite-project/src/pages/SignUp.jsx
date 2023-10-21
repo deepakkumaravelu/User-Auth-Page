dc4bdbf654126990c0c6b38379d8d6ea363b83cd
deepak <deepak1122003kumar@gmail.com>
2023-10-21 21:57:37 +0530

    Sigup page updated

--- frontend/vite-project/src/pages/SignUp.jsx
+++ frontend/vite-project/src/pages/SignUp.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import { useState } from 'react'
+import {Container,Form,Button} from "react-bootstrap"
+import '../styles/SignUp.css'
+import {Link,useNavigate} from "react-router-dom"
 const SignUp = () => {
+    const [formData,setFormData]=useState({
+name:"",
+email:"",
+password:""
+    })
+    const handleChange=()=>{
+        
+    }
   return (
-    <div>SignUp</div>
+    <Container>
+         <h1>Registration Form</h1>
+             <Form>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text " name="name" value={formData.name} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name="email" value={formData.email} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password " name="password" value={formData.password} onChange={handleChange} required />
+                </Form.Group>
+                <Button variant='primary' type='submit'>Register</Button>
+                {/* <p>Already have an account</p> */}
+                {/* <Link to="/login">Login</Link> */}
+             </Form>
+    </Container>
   )
 }
 
